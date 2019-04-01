accc38d85294bb790be35dce46f43ef15c792fb2
Ramiroqr <ramiroquisper@gmail.com>
2019-04-01 12:43:51 -0500

    binding

---
+++ Lands5/Lands5/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,23 @@
+﻿namespace Lands5.Infrastructure
+{
+    using Lands5.ViewModels;
+
+    public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator() 
+        { 
+            //Ligaremos la pagina login con la Mainviewmodel
+            this.Main  = new MainViewModel();
+        }
+        #endregion
+    }
+}
