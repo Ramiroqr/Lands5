d53fd7eee05de3fd899597bc0bc07c5682ec7a92
Ramiroqr <ramiroquisper@gmail.com>
2019-04-02 08:00:11 -0500

    LoginPage

---
+++ Lands5/Lands5/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands5.ViewModels
+{
+    class MainViewModel
+    {
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+    }
+}
