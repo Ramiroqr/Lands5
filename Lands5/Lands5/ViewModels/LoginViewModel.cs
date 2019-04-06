fe9077454eb878ef05efcfc7973b66a77ef382dd
Ramiroqr <ramiroquisper@gmail.com>
2019-04-05 21:09:39 -0500

    propiedades login

--- Lands5/Lands5/ViewModels/LoginViewModel.cs
+++ Lands5/Lands5/ViewModels/LoginViewModel.cs
@@ -1,7 +1,11 @@
 ﻿
 namespace Lands5.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using System;
     using System.Windows.Input;
+    using Xamarin.Forms;
+
     class LoginViewModel
     {
         #region Properties
@@ -12,7 +16,48 @@ namespace Lands5.ViewModels
         #endregion
 
         #region Commands
-        public ICommand  LoginCommand { get; set; }
+        public ICommand  LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+            if(string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email",
+                    "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an password",
+                    "Accept");
+                return;
+            }
+
+            if (this.Email != "r@gmail.com" || this.Password != "123")
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or password incorrect.",
+                    "Accept");
+                return;
+            }
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "Correct.",
+                    "Accept");
+        }
         #endregion
 
         #region Constructors
